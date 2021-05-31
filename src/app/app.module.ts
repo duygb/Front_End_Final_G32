78325d2657adf573124702dff7c9c6138077c3fa
huynhgiahuy0409 <18130094@st.hcm.uaf.edu.vn>
2021-05-31 14:38:51 +0700

    create header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShoppingCartComponent
+    ShoppingCartComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
