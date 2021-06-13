6d7e4821c73c4df09934661cad90132b29b22b9a
Hoang Quoc Duy <18130055@st.hcmuaf.edu.vn>
2021-06-13 20:02:05 +0700

    product

---
+++ src/app/product/product-list/product-list.component.ts
@@ -0,0 +1,54 @@
+
+import { Product } from './../../../common-module/product';
+import { Pagination } from './../../../common-module/pagination';
+import { Component, OnInit, Input, Output, EventEmitter, DoCheck } from '@angular/core';
+import { SaleProduct } from 'src/common-module/sale-product';
+import { MyServerHttpService } from 'src/app/Services/my-server-http-service.service';
+import { Router } from '@angular/router';
+import { Sorter } from './common-product/sorter';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.scss']
+})
+export class ProductSaleListComponent implements OnInit {
+  @Input() public allSaleProducts!: SaleProduct[];
+  @Input() public saleProducts!: SaleProduct[];
+  @Input() public pagination!: Pagination;
+  @Input() public sorters!: Sorter[];
+  @Input() public sortCheck!: any;
+  @Input() public visiblePagesNumber!: any;
+
+  @Output() onChanged = new EventEmitter();
+  @Output() onFirstPage = new EventEmitter();
+  @Output() onLastPage = new EventEmitter();
+  @Output() onPreviousPage = new EventEmitter();
+  @Output() onNextPage = new EventEmitter();
+  @Output() onIndexPaginationChange = new EventEmitter();
+  constructor(
+    private myServerHttpService: MyServerHttpService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {}
+  indexPaginationChange(valueChange: number) {
+    this.onIndexPaginationChange.emit(valueChange);
+  }
+  changed(selectElement: HTMLSelectElement){
+    this.onChanged.emit(selectElement);
+  }
+  previousPage() {
+    this.onPreviousPage.emit();
+  }
+  nextPage() {
+    this.onNextPage.emit();
+  }
+  firstPage() {
+    this.onFirstPage.emit();
+  }
+  lastPage() {
+    this.onLastPage.emit();
+  }
+}
+
