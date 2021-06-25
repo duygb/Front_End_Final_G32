348973042d6c13c964af4bab99a8e727b53a4262
huynhgiahuy0409 <18130094@st.hcm.uaf.edu.vn>
2021-06-25 23:51:25 +0700

    .

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,36 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
-=======
->>>>>>> parent of 2f5711f (handling login)
-=======
->>>>>>> parent of 2f5711f (handling login)
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-<<<<<<< HEAD
-<<<<<<< HEAD
   constructor() {}
   ngOnInit(): void {
     throw new Error('Method not implemented.');
-=======
-=======
->>>>>>> parent of 2f5711f (handling login)
-
-  constructor() { }
-
-  ngOnInit(): void {
-<<<<<<< HEAD
->>>>>>> parent of 2f5711f (handling login)
-=======
->>>>>>> parent of 2f5711f (handling login)
   }
-
+  
 }
