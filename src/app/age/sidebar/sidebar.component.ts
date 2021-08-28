e6b6bd09929e45b6e7bb82b3a2c6208a5cd50b42
lamhaison2000 <73637656+lamhaison2000@users.noreply.github.com>
2021-08-29 04:08:21 +0700

    .

--- src/app/age/sidebar/sidebar.component.ts
+++ src/app/age/sidebar/sidebar.component.ts
@@ -10,63 +10,8 @@ import { Sex } from './common/sex';
   styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit {
-  brands: Brand[] = [];
-  ages: Age[] = [];
-  sexs: Sex[] = [];
-  allElement: string[] = ['brands', 'ages', 'sexs'];
-  checkAge: number[] = [];
-  searchValue: string = '';
-
-
-  @Output() onClickAge = new EventEmitter();
-  @Output() onClickSex = new EventEmitter();
-  @Output() onClickBrand = new EventEmitter();
-  @Output() onClickSearch = new EventEmitter();
-
-  constructor(private myHttp: MyServerHttpService) {}
   ngOnInit(): void {
-    /*  this.setData(this.allElement); */
-    this.brands = this.setData(this.allElement);
+    throw new Error('Method not implemented.');
   }
-  public clickChooseAge(id: number) {
-    if (this.checkAge.includes(id)) {
-      this.checkAge.splice(id);
-      alert('Đã xoá');
-      alert(this.checkAge);
-    } else {
-      this.checkAge.push(id);
-      alert('Đã thêm');
-      alert(this.checkAge);
-    }
-  }
-  public setData(serverPaths: string[]): any {
-    serverPaths.forEach((element) => {
-      if (element === 'brands') {
-        this.myHttp.getAll(element).subscribe((data) => {
-          this.brands = data as Brand[];
-        });
-      } else if (element === 'ages') {
-        this.myHttp.getAll(element).subscribe((data) => {
-          this.ages = data as Age[];
-        });
-      } else if (element === 'sexs') {
-        this.myHttp.getAll(element).subscribe((data) => {
-          this.sexs = data as Sex[];
-        });
-      }
-    });
-  }
-  /* -----Sidebar Event----- */
-  public clickAge(ageObject: Age){
-    this.onClickAge.emit(ageObject);
-  }
-  public clickSex(sexObject: Sex){
-    this.onClickSex.emit(sexObject);
-  }
-  public clickBrand(brandObject: Brand){
-    this.onClickBrand.emit(brandObject);
-  }
-  public clickSearch(){
-    this.onClickSearch.emit(this.searchValue);
-  }
+
 }
